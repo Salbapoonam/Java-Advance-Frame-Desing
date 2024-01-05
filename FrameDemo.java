fb8f2c251b1fc530b824878d97861cc91e932461
Poonam Kisan Salbande <97495345+Salbapoonam@users.noreply.github.com>
2024-01-05 16:03:42 +0530

    Add files via upload

---
+++ FrameDemo.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+ public class FrameDemo extends Frame
+{
+     public static void main(String args[])
+	 {
+	  FrameDemo f1=new FrameDemo();
+	  f1.setVisible(true);
+	  f1.setSize(500,500);
+	  f1.setTitle("VJ Tech Frame");
+	 }
+}
